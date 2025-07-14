2309e731db49d550d022c0afd5ed47e36223930b
faisyou <faisal.yousuf@gmail.com>
2025-07-14 14:06:27 +0400

    Initial Commit

---
+++ src/services/secret-service.ts
@@ -0,0 +1,22 @@
+/*
+* This class provides the authentication process the secret for a given key.
+*/
+export class SecretService {
+    /*
+    * For this implementation, we are just using a static map that holds the key/values.
+    * In reality, you will want to store these somewhere else, like S3, or some other
+    * secrets manager.
+    */
+    static secrets = new Map();
+
+    static {
+        SecretService.secrets.set('ApiKey1', 'U2VjcmV0MQ=='); // U2VjcmV0MQ==
+    }
+
+    getSecretForKey(key: string): Uint8Array {
+        const secretString = SecretService.secrets.get(key) || '';
+
+        //return Buffer.from(secretString);
+        return Buffer.from(secretString, 'base64');
+    }
+}
